8960122df5fa6a146847b10f339081970277d061
Kifkof <cosimo.augustoni@gmail.com>
2016-12-14 14:22:01 +0100

    Added new StringIsNumeric Validation to BaseValidator

--- src/SpielGut/Helpers/Validators/BaseValidator.cs
+++ src/SpielGut/Helpers/Validators/BaseValidator.cs
@@ -1,4 +1,5 @@
-﻿using System.Text.RegularExpressions;
+﻿using System.Linq;
+using System.Text.RegularExpressions;
 using Helpers.BaseTypes;
 
 
@@ -13,26 +14,36 @@ namespace Helpers.Validators
         {
             return !string.IsNullOrWhiteSpace(toValidate);
         }
+
         protected bool StringIsInMaxLenght(string toValidate, int max)
         {
             return this.StringIsInRange(toValidate, 0, max);
         }
+
         protected bool StringIsInRange(string toValidate, int min, int max)
         {
             return toValidate.Length >= min && toValidate.Length <= max;
         }
+
         protected bool StringIsEmail(string toValidate)
         {
             string emailRegex = "^(?:(?:[\\w`~!#$%^&*\\-=+;:{}\'|,?\\/]+(?:(?:\\.(?:\"(?:\\\\?[\\w`~!#$%^&*\\-=+;:{}\'|,?\\/\\.()<>\\[\\] @]|\\\\\"|\\\\\\\\)*\"|[\\w`~!#$%^&*\\-=+;:{}\'|,?\\/]+))*\\.[\\w`~!#$%^&*\\-=+;:{}\'|,?\\/]+)?)|(?:\"(?:\\\\?[\\w`~!#$%^&*\\-=+;:{}\'|,?\\/\\.()<>\\[\\] @]|\\\\\"|\\\\\\\\)+\"))@(?:[a-zA-Z\\d\\-]+(?:\\.[a-zA-Z\\d\\-]+)*|\\[\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\])$";
             return this.StringMatchesRegEx(toValidate, emailRegex);
         }
+
         protected bool StringMatchesRegEx(string toValidate, string regex)
         {
             return Regex.IsMatch(toValidate, regex);
         }
+
         protected bool StringsAreEqual(string stringA, string stringB)
         {
             return stringA == stringB;
+        }
+
+        protected bool StringIsNumeric(string toValidate)
+        {
+            return toValidate.All(char.IsDigit);
         }
 
     }
