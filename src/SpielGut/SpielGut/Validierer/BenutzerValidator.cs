109ee8cb81a2a1d7224ccb3e9931bada0c107553
Kifkof <cosimo.a@gmx.net>
2016-12-17 16:01:12 +0100

    commit und so

--- src/SpielGut/SpielGut/Validierer/BenutzerValidator.cs
+++ src/SpielGut/SpielGut/Validierer/BenutzerValidator.cs
@@ -1,4 +1,5 @@
-﻿using KDG.DataObjectHandler.Validators;
+﻿using System.Collections.Generic;
+using KDG.DataObjectHandler.Validators;
 using SpielGut.Klassen;
 
 namespace SpielGut.Validierer
@@ -7,52 +8,119 @@ namespace SpielGut.Validierer
     {
         private static Benutzer user;
 
-        public static bool IsValid(Benutzer userToValidate)
+        private static List<string> errorMessages;
+
+        public static List<string> IsValid(Benutzer userToValidate)
         {
             user = userToValidate;
-            return FirstNameIsValid() &&
-                   SurnameIsValid() &&
-                   EmailIsValid() &&
-                   PasswordIsValid() &&
-                   AddressIsValid() &&
-                   PhoneNumberIsValid();
+            errorMessages = new List<string>();
+            errorMessages.AddRange(FirstNameIsValid());
+            errorMessages.AddRange(SurnameIsValid());
+            errorMessages.AddRange(EmailIsValid());
+            errorMessages.AddRange(PasswordIsValid());
+            errorMessages.AddRange(AddressIsValid());
+            errorMessages.AddRange(PhoneNumberIsValid());
+            return errorMessages;
         }
 
-        private static bool FirstNameIsValid()
+        private static List<string> FirstNameIsValid()
         {
-            return BaseValidator.StringHasValue(user.Vorname) &&
-                   BaseValidator.StringIsInMaxLenght(user.Vorname, 40);
+            var messages = new List<string>();
+            if (BaseValidator.StringHasValue(user.Vorname))
+            {
+                messages.Add("Das Feld Vorname muss ausgefüllt sein.");
+            }
+            if (BaseValidator.StringIsInMaxLenght(user.Vorname, 40))
+            {
+                messages.Add("Das Feld Vorname darf maximal 40 Zeichen beinhalten.");
+            }
+            return messages;
         }
 
-        private static bool SurnameIsValid()
+        private static List<string> SurnameIsValid()
         {
-            return BaseValidator.StringHasValue(user.Nachname) &&
-                   BaseValidator.StringIsInMaxLenght(user.Nachname, 40);
+            var messages = new List<string>();
+            if (BaseValidator.StringHasValue(user.Nachname))
+            {
+                messages.Add("Das Feld Nachname muss ausgefüllt sein.");
+            }
+            if (BaseValidator.StringIsInMaxLenght(user.Nachname, 40))
+            {
+                messages.Add("Das Feld Nachname darf maximal 40 Zeichen beinhalten.");
+            }
+            return messages;
         }
-        private static bool EmailIsValid()
+        private static List<string> EmailIsValid()
         {
-           
-            return BaseValidator.StringHasValue(user.Email) &&
-                   BaseValidator.StringIsEmail(user.Email) &&
-                   BaseValidator.StringIsInMaxLenght(user.Email, 100);
+            var messages = new List<string>();
+            if (BaseValidator.StringHasValue(user.Email))
+            {
+                messages.Add("Das Feld Email muss ausgefüllt sein.");
+                if (BaseValidator.StringIsEmail(user.Email))
+                {
+                    messages.Add("Das Feld Email beinhaltet keine gültige Email.");
+                }
+            }
+            if (BaseValidator.StringIsInMaxLenght(user.Email, 100))
+            {
+                messages.Add("Das Feld Email darf maximal 100 Zeichen beinhalten.");
+            }
+            return messages;
         }
-        private static bool PasswordIsValid()
+        private static List<string> PasswordIsValid()
         {
-            return BaseValidator.StringHasValue(user.Passwort);
+            var messages = new List<string>();
+            if (BaseValidator.StringHasValue(user.Passwort))
+            {
+                messages.Add("Das Feld Nachname muss ausgefüllt sein.");
+            }
+            return messages;
         }
-        private static bool AddressIsValid()
+        private static List<string> AddressIsValid()
         {
-            //TODO: ADDRESS VALIDATE
-            return true;
-            return BaseValidator.StringHasValue(user.Vorname) &&
-                   BaseValidator.StringIsInMaxLenght(user.Vorname, 40);
+            var messages = new List<string>();
+            if (BaseValidator.StringHasValue(user.Address.Strasse))
+            {
+                messages.Add("Das Feld Strasse muss ausgefüllt sein.");
+            }
+            if (BaseValidator.StringHasValue(user.Address.Ort))
+            {
+                messages.Add("Das Feld Ort muss ausgefüllt sein.");
+            }
+            if (BaseValidator.StringHasValue(user.Address.Postleitzahl))
+            {
+                messages.Add("Das Feld Postleitzahl muss ausgefüllt sein.");
+            }
+            if (BaseValidator.StringIsInMaxLenght(user.Address.Strasse, 40))
+            {
+                messages.Add("Das Feld Strasse darf maximal 40 Zeichen beinhalten.");
+            }
+            if (BaseValidator.StringIsInMaxLenght(user.Address.Ort, 40))
+            {
+                messages.Add("Das Feld Ort darf maximal 40 Zeichen beinhalten.");
+            }
+            if (BaseValidator.StringIsInMaxLenght(user.Address.Postleitzahl, 4))
+            {
+                messages.Add("Das Feld Postleitzahl darf maximal 4 Zeichen beinhalten.");
+            }
+            if (BaseValidator.StringIsNumeric(user.Address.Postleitzahl))
+            {
+                messages.Add("Das Feld Postleitzahl darf nur Zahlen beinhalten.");
+            }
+            return messages;
         }
-        private static bool PhoneNumberIsValid()
+        private static List<string> PhoneNumberIsValid()
         {
-            //TODO: PHONE VALIDATE
-            return true;
-            return BaseValidator.StringHasValue(user.Vorname) &&
-                   BaseValidator.StringIsInMaxLenght(user.Vorname, 40);
+            var messages = new List<string>();
+            if (BaseValidator.StringHasValue(user.Telefonnummer))
+            {
+                messages.Add("Das Feld Telefonnummer muss ausgefüllt sein.");
+            }
+            if (BaseValidator.StringIsInMaxLenght(user.Telefonnummer, 40))
+            {
+                messages.Add("Das Feld Telefonnummer darf maximal 40 Zeichen beinhalten.");
+            }
+            return messages;
         }
     }
 }
--- src/SpielGut/SpielGut/Validierer/PasswortValidator.cs
+++ src/SpielGut/SpielGut/Validierer/PasswortValidator.cs
@@ -1,16 +1,34 @@
-﻿using KDG.DataObjectHandler.Validators;
+﻿using System.Collections.Generic;
+using KDG.DataObjectHandler.Validators;
 using SpielGut.Klassen;
 
 namespace SpielGut.Validierer
 {
     public static class PasswortValidator
     {
-        public static bool IsValid(string passwortA, string passwortB)
+        public static List<string> IsValid(string passwortA, string passwortB)
         {
-            return BaseValidator.StringHasValue(passwortA) &&
-                   BaseValidator.StringHasValue(passwortB) &&
-                   BaseValidator.StringIsInRange(passwortA, 8, 64) &&
-                   BaseValidator.StringsAreEqual(passwortA, passwortB);
+            var messages = new List<string>();
+            if (BaseValidator.StringHasValue(passwortA))
+            {
+                messages.Add("Das Feld 'Passwort' muss ausgefüllt sein.");
+            }
+            if (BaseValidator.StringHasValue(passwortB))
+            {
+                messages.Add("Das Feld 'Passwort Wiederholen' muss ausgefüllt sein.");
+            }
+            if (messages.Count == 0)
+            {
+                if (BaseValidator.StringIsInRange(passwortA, 8, 64))
+                {
+                    messages.Add("Das Passwort muss zwischen 8 und 64 Zeichen lang sein.");
+                }
+                if (BaseValidator.StringsAreEqual(passwortA, passwortB))
+                {
+                    messages.Add("Die Passwörter stimmen nicht überein.");
+                }
+            }
+            return messages;
         }
     }
 }
